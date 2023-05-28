5d4d182fc1a9252298f1bac596d8ce3d952dcfdd
Shivani Mishra <119618398+SHIVANIMISHRA123@users.noreply.github.com>
2023-05-28 14:14:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Chart from "./components/Chart";
+import Navbar from "./components/Navbar";
+import Typing from "./components/Typing";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/v-type" element={<Typing />} />
+        <Route path="/result" element={<Chart />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
